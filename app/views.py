07f6a43ed200f887287d051ddf8e63e71e6f58ad
cardozoleonardo <leonardocardozo699@gmail.com>
2024-11-27 01:44:39 -0300

    optimizando views

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from app.layers.transport import transport
 
 def index_page(request):
     return render(request, 'index.html')
@@ -12,10 +11,7 @@ def index_page(request):
 # esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
-    images = []
-    
-    for objeto in services.getAllImages(None):
-        images.append(objeto)
+    images = services.getAllImages(None)
    
     favourite_list = []
     favourite_list.append(services.getAllFavourites)
